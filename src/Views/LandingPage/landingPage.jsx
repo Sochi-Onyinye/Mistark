59ddd721c2381e31b803f9d92619230b57edf1d8
Sochi-Onyinye <osogbuanya@gmail.com>
2023-02-20 00:17:05 -0500

    landing Page

---
+++ src/Views/LandingPage/landingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landingPage.css';
+
+const landingPage = () => {
+  return (
+    <div>
+      <h1>"Just Landed"</h1>
+    </div>
+  )
+}
+
+export default landingPage;
