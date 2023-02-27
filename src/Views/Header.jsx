e8d7f1647d7ef4a012c454f3cd92ea184a53484b
ToluAkinlabi <35533505+ToluAkinlabi@users.noreply.github.com>
2023-02-26 23:52:31 -0500

    updated header

--- src/Views/Header.jsx
+++ src/Views/Header.jsx
@@ -5,9 +5,17 @@ import './Header.css'
 class Header extends React.Component{
   render() {
     return (
-      <div class="mask">
-        <header class="header d-flex"></header>
+      <div class="mask header">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Discover a Business</h1>
+          <h4 class="mb-3">Subheading</h4>
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+          >Call to action</a
+          >
+        </div>
       </div>
+    </div>
     );
   }
 }
