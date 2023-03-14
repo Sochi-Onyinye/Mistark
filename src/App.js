f2ef370eaa2cc837e212d99fada28abc81a3b39e
ToluAkinlabi <35533505+ToluAkinlabi@users.noreply.github.com>
2023-03-14 14:24:12 -0400

    updated pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Views/LandingPage';
 import NavBar from './Views/NavBar';
 import UserLogin from './Views/UserLogin';
+import BusinessLogin from './Views/BusinessLogin';
 import UserRegistration from './Views/UserRegistration';
+import UserFeed from "./Views/UserFeed";
 
 
 
@@ -15,6 +17,8 @@ const App = () => (
         <Route path="/registration" element={<UserRegistration/>} />
         <Route path="/navbar" element={<NavBar/>} />
         <Route path="/landingpage" element={<LandingPage/>} />
+        <Route path="/userfeed" element={<UserFeed/>} />
+        <Route path="/businesslogin" element={<BusinessLogin/>} />
       </Routes>
     
   </React.StrictMode>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import "./Fonts/Geliat-ExtraBold.otf"
+import "./Fonts/Geliat-ExtraLight.otf"
 import "./index.css";
 import App from "./App";
 
