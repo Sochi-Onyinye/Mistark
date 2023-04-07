29ed4ea207739280cc4d96f15674fdbd381ce1d9
Sochi-Onyinye <osogbuanya@gmail.com>
2023-04-07 14:51:12 -0400

    update to usergriditem

--- src/Views/UserFeedGridItem.jsx
+++ src/Views/UserFeedGridItem.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 export function UserFeedGridItem(props){
-const {businessname, description, onClick} = props
+const {businessname, description, onClick, businessImageUrl} = props
 return (
     <div class="col-9 card bg-success" id="userfeedcard" onClick={onClick}>
         {/* Everything you need to do for the user feed grid should be done within this div*/ }
         <div class="card-body">
-            <h5 class="card-title" id="card-title">{businessname}</h5>
-            <h5 class="card-subtitle mb-2" id="card-subtitle">{description}</h5>
+            <h5 class="card-title" id="card-title"></h5>
+            <img src={businessImageUrl} alt="" />
+            <h5 class="card-subtitle mb-2" id="card-subtitle">{businessname}</h5>
             <p class="card-text">More Info</p>
         </div>  
         {/* end of div*/ }
     </div>
-    
 )
 }
+
 
