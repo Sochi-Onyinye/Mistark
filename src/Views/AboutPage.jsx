a3e039c7b1d5d839e88102972ed9547b9daa8802
Sochi-Onyinye <osogbuanya@gmail.com>
2023-04-09 18:21:21 -0400

    about page and help page updated

--- src/Views/AboutPage.jsx
+++ src/Views/AboutPage.jsx
@@ -9,6 +9,8 @@ class AboutPage extends React.Component{
         <div>
             <NavPanel/>
             <h3 id="about">About Us</h3>
+            <p id="story">Our product is a web app that matches prospective customers to existing minority-owned businesses. This product addresses two types of users: members of the public seeking businesses and business owners looking to be found. Users are able to find businesses via multiple methods, including proximity to the user's current location, text keywords eg colleges, product images, voice search, and recommendations based on search history or behavior.
+            </p>
         </div>
 
         );
--- src/Views/HelpPage.jsx
+++ src/Views/HelpPage.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import NavPanel from "./NavPanel";
 import './HelpPage.css' 
+import background from '../others/background.png'
+import { Link } from 'react-router-dom';
+import { XSquareFill } from "react-bootstrap-icons";
+import LandingPage from "./LandingPage";
 
 class HelpPage extends React.Component{
     render() {
       return (
         <div>
             <NavPanel/>
-            <h3 id="help">Help page - team contact info *IT</h3>
+            <div class="bg">
+                <img src={background} alt="" />
+            </div>
+            <div className="overlay container form-group needs-validation">
+                <div className='d-flex justify-content-center align-items-center h-100'>
+                    <div className= "col-12">
+                        <div className="card loginCard">
+                            <div className="p-5 d-flex flex-column align-items-center w-100">
+                                <Link to="/" component={LandingPage} id="close"><XSquareFill/></Link>
+                                <h5>Please Contact Us here:</h5>
+                                <h6>IT support: phone number </h6>
+                                <h6>Customer care: phone number </h6>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         );
