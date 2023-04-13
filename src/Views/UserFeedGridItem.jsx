6ac6d7dda63624be5500c95399cf948319768055
ToluAkinlabi <35533505+ToluAkinlabi@users.noreply.github.com>
2023-04-13 12:52:56 -0400

    updated styling

--- src/Views/UserFeedGridItem.jsx
+++ src/Views/UserFeedGridItem.jsx
@@ -6,15 +6,18 @@ const {businessName, onClick, profileImage} = props
 RecordPageView()
 
 return (
-    <div class="col-9 card bg-success" id="userfeedcard" onClick={onClick}>
+    <div className="card bg-success" id="userfeedcard" onClick={onClick}>
         {/* Everything you need to do for the user feed grid should be done within this div*/ }
         <div class="card-body">
             <h5 class="card-title" id="card-title"></h5>
-            <img src={profileImage} alt="" />
-            <h5 class="card-subtitle mb-2" id="card-subtitle">{businessName}</h5>
+            <div className='row'>
+                <img src={profileImage} alt="" id="userfeedimg" className='col-4'/>
+                <h5 className="card-subtitle mb-2 col-8" id="card-subtitle">{businessName}</h5>
+            </div>
         </div>  
         {/* end of div*/ }
-    </div> )
+    </div> 
+    )
 }
 
 
